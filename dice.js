8611e96a0b9138ac648aa29aa102cb41f3b5ef8a
ChungGold <74089047+ChungGold@users.noreply.github.com>
2020-12-20 13:32:43 -0800

    Add files via upload

--- dice.js
+++ dice.js
@@ -1,17 +1,26 @@
 const roll1 = Math.ceil(6*Math.random());
 const roll2 = Math.ceil(6*Math.random());
 re(roll1, roll2);
+var counter = 6;
 
 function diceButton() {
+    counter = 6;
+    playDice();
+    diceLoop = setInterval(playDice, 250);
+    setTimeout(winner, 1550,roll1, roll2) 
+}
+
+function playDice() {
     const roll1 = Math.ceil(6*Math.random());
     const roll2 = Math.ceil(6*Math.random());
     re(roll1, roll2);
-    winner(roll1, roll2);
+    counter--;
+    if (counter === 0) {
+    clearInterval(diceLoop);
+    } 
 }
 
 function re(roll1, roll2) {
-    console.log("This is roll 1: ", roll1);
-    console.log("This is roll 2: ", roll2);
     switch(roll1){
         case 1: 
             document.getElementsByClassName('img1')[0].src='images/dice1.png';
@@ -53,11 +62,23 @@ function re(roll1, roll2) {
 }
 
 function winner(roll1, roll2){
+    setTimeout(winner, 1500,roll1, roll2)
     if (roll1 > roll2){
-        document.querySelector('h1').textContent = 'Player 1 Wins!';  
+        document.querySelector('h1').textContent = 'Player 1 Wins!'; 
     } else if (roll1 < roll2){
         document.querySelector('h1').textContent = 'Player 2 Wins!';
     } else if (roll1 = roll2){
         document.querySelector('h1').textContent = 'It is a tie!';
     }
 }
+
+/* The answer on Udemy
+var roll1 = Math.floor(Math.random() * 6) + 1;
+var roll1Image = "dice" + roll1 + ".png";
+var roll1Source = "images/" + roll1Image;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", roll1Source);
+
+var roll2 = Math.floor(Math.random() * 6) + 1;
+var roll2Source2 - "images/dice" + roll2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); */
